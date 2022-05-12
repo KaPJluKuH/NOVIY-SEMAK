0465747f2c5b6ee968909549ef6669ec1ca7b071
KaPJluKuH <franklinzaaz@yandex.ru>
2022-05-13 01:34:10 +0300

    debug

--- proxy.py
+++ proxy.py
@@ -18,13 +18,40 @@ def get_free_proxies():
             proxies.append(host)
         except IndexError:
             continue
+    print(proxies)
     return proxies
 
 
 def main_proxy():
     free_proxies = get_free_proxies()
-    return choice(free_proxies)
+    my_proxy = choice(free_proxies)
+    http_proxy = f"http://{my_proxy}"
+    https_proxy = f"https://{my_proxy}"
+    ftp_proxy = f"ftp://{my_proxy}"
+    proxies = {
+        "http": http_proxy,
+        "https": https_proxy,
+        "ftp": ftp_proxy
+    }
+    print(proxies)
+    return proxies
     # print(f'Обнаружено бесплатных прокси - {len(free_proxies)}:')
     # for i in range(len(free_proxies)):
     #     print(f"{i+1}) {free_proxies[i]}")
 
+
+# def get_session(proxies):
+#     # создать HTTP‑сеанс
+#     session = requests.Session()
+#     # выбираем один случайный прокси
+#     proxy = random.choice(proxies)
+#     session.proxies = {"http": proxy, "https": proxy}
+#     return session
+#
+#
+# for i in range(5):
+#     s = get_session(proxies)
+#     try:
+#         print("Страница запроса с IP:", s.get("http://icanhazip.com", timeout=1.5).text.strip())
+#     except Exception as e:
+#         continue
